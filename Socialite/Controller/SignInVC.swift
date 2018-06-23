3c4cf6e48c37b23dec07d4d7e7427e6284c6f47b
Imanuel Pardosi <ipardosi@dyned.com>
2018-06-23 21:49:02 +0700

    Integrated with Firebase

---
+++ Socialite/Controller/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Socialite
+//
+//  Created by Mobile Jakarta Team on 23/06/18.
+//  Copyright © 2018 moonshadow. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
